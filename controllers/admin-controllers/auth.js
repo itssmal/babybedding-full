27430a526ac25b57c554d493ed30c1d7c3decf5c
Sasha <wwesasha12@gmail.com>
2020-07-10 23:51:51 +0300

    register added

--- controllers/admin-controllers/auth.js
+++ controllers/admin-controllers/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 const keys = require('../../config/keys')
 
 const User = require('../../models/User')
@@ -8,12 +9,13 @@ module.exports.login = async function(req, res) {
 
 
     if (candidate) {
-        if (req.body.password === candidate.password) {
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+        if (passwordResult) {
             // gen token, passwords match
             const token = jwt.sign({
                 email: req.body.email,
                 userId: candidate._id,
-            }, keys.jwt, {expiresIn: 3600})
+            }, keys.jwt, {expiresIn: 21600})
 
             res.status(200).json({
                 token: `Bearer ${token}`
@@ -28,5 +30,31 @@ module.exports.login = async function(req, res) {
         res.status(404).json({
             message: 'Користувача з такою поштовою адресою не існує'
         })
+    }
+}
+
+module.exports.register = async function (req, res) {
+
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        res.status(409).json({
+            message: 'Користувач з такою поштовою адресою вже існує!'
+        })
+    } else {
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt),
+        })
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch (e) {
+            res.status(404).json({
+                message: e.error.message
+            })
+        }
     }
 }
--- routes/admin-routes/auth.js
+++ routes/admin-routes/auth.js
@@ -3,8 +3,8 @@ const controller = require('../../controllers/admin-controllers/auth')
 const router = express.Router()
 
 
-//http://localhost:5000/api/auth/login
-
 router.post('/login', controller.login)
+
+router.post('/register', controller.register)
 
 module.exports = router
