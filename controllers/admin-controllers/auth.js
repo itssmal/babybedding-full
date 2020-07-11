013414ba47bb6fb72664500b5c504993ebc84780
Sasha <wwesasha12@gmail.com>
2020-07-11 07:17:52 +0300

    registration commented out

--- controllers/admin-controllers/auth.js
+++ controllers/admin-controllers/auth.js
@@ -31,28 +31,28 @@ module.exports.login = async function(req, res) {
     }
 }
 
-module.exports.register = async function (req, res) {
-
-    const candidate = await User.findOne({email: req.body.email})
-
-    if (candidate) {
-        res.status(409).json({
-            message: 'Користувач з такою поштовою адресою вже існує!'
-        })
-    } else {
-        const salt = bcrypt.genSaltSync(10)
-        const password = req.body.password
-        const user = new User({
-            email: req.body.email,
-            password: bcrypt.hashSync(password, salt),
-        })
-        try {
-            await user.save()
-            res.status(201).json(user)
-        } catch (e) {
-            res.status(404).json({
-                message: e.error.message
-            })
-        }
-    }
-}
+// module.exports.register = async function (req, res) {
+//
+//     const candidate = await User.findOne({email: req.body.email})
+//
+//     if (candidate) {
+//         res.status(409).json({
+//             message: 'Користувач з такою поштовою адресою вже існує!'
+//         })
+//     } else {
+//         const salt = bcrypt.genSaltSync(10)
+//         const password = req.body.password
+//         const user = new User({
+//             email: req.body.email,
+//             password: bcrypt.hashSync(password, salt),
+//         })
+//         try {
+//             await user.save()
+//             res.status(201).json(user)
+//         } catch (e) {
+//             res.status(404).json({
+//                 message: e.error.message
+//             })
+//         }
+//     }
+// }
--- routes/admin-routes/auth.js
+++ routes/admin-routes/auth.js
@@ -5,6 +5,6 @@ const router = express.Router()
 
 router.post('/login', controller.login)
 
-router.post('/register', controller.register)
+// router.post('/register', controller.register)
 
 module.exports = router
